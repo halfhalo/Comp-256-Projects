3f3797c0c6a49bd41e963b9edf3e6cafb7f6191d
Patrick Roberts <patrick@silentbluesystems.com>
2012-03-07 09:31:14 -0800

    Test H

--- Project2/P4.java
+++ Project2/P4.java
@@ -1,3 +1,9 @@
+/*
+	Patrick Roberts
+	Comp 256
+	March 7,2012
+	Source code repo: https://github.com/halfhalo/Comp-256-Projects
+*/
 public class P4 implements LogicalFunction
 {
       public boolean evaluate( boolean p, boolean q, boolean r)
